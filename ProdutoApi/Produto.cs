c781ae26e4e7ed2b1a70e73d3d25c2b981dcaeea
Paulo Piaunios <paulosantossjc@hotmail.com>
2024-09-10 13:39:04 -0300

    Removendo classes criadas na raiz do projeto

--- ProdutoApi/Produto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProdutoApi.Domain.Entities;
-
-public class Produto
-{
-    public Guid Id { get; set; }
-    public string Nome { get; set; }
-    public decimal Preco { get; set; }
-    public string MoedaOrigem { get; set; }
-}
